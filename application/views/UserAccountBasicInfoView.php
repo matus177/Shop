db34c8dbc0af85f48523d7b3939220aa1d40f4c8
matandre123 <matus.andrejcak@t-systems.sk>
2016-12-09 16:00:37 +0100

    [+] added Facture data to basic user settings menu

--- application/views/UserAccountBasicInfoView.php
+++ application/views/UserAccountBasicInfoView.php
@@ -1,6 +1,6 @@
 <div class="col-md-9">
     <?php $this->load->view('FlashMessagesView'); ?>
-    <table class="">
+    <table id="table_basic_info">
         <tbody>
         <tr>
             <td>
@@ -37,6 +37,93 @@
     </table>
     <div class="aaaa">
         <p>asdadad</p>
+    </div>
+    <hr>
+    <p><b>Fakturačné údaje</b></p>
+    <table id="table_facture_info" class="table_size">
+        <tbody>
+        <tr>
+            <td>
+                IČO
+                <input type="text" id="ico" class="form-control" name="comp_ico"
+                       value="<?php echo $data['comp_ico'] ?>">
+            </td>
+            <td>
+                <div class="inline_inputs">
+                    IČ DPH
+                    <input type="text" id="icdph" class="form-control" name="comp_icdph"
+                           value="<?php echo $data['comp_icdph'] ?>">
+                </div>
+            </td>
+            <td>
+                <div class="inline_inputs">
+                    DIČ
+                    <input type="text" id="dic" class="form-control" name="comp_dic"
+                           value="<?php echo $data['comp_dic'] ?>">
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td class="format_inputs_top">
+                <div class="required-fields"><label for="email" class="control-label">Meno</label></div>
+                <input type="text" id="first_name" class="form-control" name="fact_name"
+                       value="<?php echo $this->encryption->decrypt($data['fact_name']) ?>">
+
+            </td>
+            <td class="format_inputs_top" colspan="2">
+                <div class="inline_inputs">
+                    Priezvisko
+                    <input type="text" id="last_name" class="form-control" name="fact_surname"
+                           value="<?php echo $this->encryption->decrypt($data['fact_surname']) ?>">
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td class="format_inputs_top">
+                Mesto
+                <input type="text" id="fact_city" class="form-control" name="fact_city"
+                       value="<?php echo $data['fact_city'] ?>">
+            </td>
+            <td class="format_inputs_top">
+                <div class="inline_inputs">
+                    Ulica
+                    <input type="text" id="fact_street" class="form-control" name="fact_street"
+                           value="<?php echo $data['fact_street'] ?>">
+                </div>
+            </td>
+            <td class="format_inputs_top">
+                <div class="inline_inputs">
+                    PSC
+                    <input type="text" id="fact_zip" class="form-control" name="fact_zip"
+                           value="<?php echo $data['fact_zip'] ?>">
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td class="format_inputs_top">
+                BIC (SWIFT)
+                <input type="text" id="comp_bic" class="form-control" name="comp_bic"
+                       value="<?php echo $data['comp_bic'] ?>">
+            </td>
+            <td class="format_inputs_top">
+                <div class="inline_inputs">
+                    IBAN
+                    <input type="text" id="comp_iban" class="form-control" name="comp_iban"
+                           value="<?php echo $this->encryption->decrypt($data['deliv_surname']) ?>">
+                </div>
+            </td>
+            <td class="format_inputs_top">
+                <div class="inline_inputs">
+                    Meno majitela uctu
+                    <input type="text" id="comp_bank_owner" class="form-control" name="comp_bank_owner"
+                           value="<?php echo $this->encryption->decrypt($data['deliv_surname']) ?>">
+                </div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    <div class="format_inputs_top">
+        <p><i class="glyphicon glyphicon-info-sign"></i> Zmeny sa ukladaju automaticky.</p>
     </div>
 </div>
 
