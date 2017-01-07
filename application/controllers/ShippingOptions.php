d15434c28be3b040667d4b42411a200497880461
matus177 <matus177@centrum.sk>
2017-01-07 21:39:07 +0100

    [+] added shipping options menu in cart

--- application/controllers/ShippingOptions.php
+++ application/controllers/ShippingOptions.php
@@ -17,4 +17,44 @@ class ShippingOptions extends CI_Controller
         $this->load->view('ShippingOptionsView');
         $this->load->view('FooterView');
     }
+
+    public function checkShippingOptions()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|xss_clean|max_length[30]');
+        $this->form_validation->set_rules('fact_name', 'Fakturacne Meno', 'trim|required|max_length[30]');
+        $this->form_validation->set_rules('fact_surname', 'Fakturacne Priezvisko', 'trim|required|max_length[30]');
+        $this->form_validation->set_rules('fact_street', 'Fakturacna Ulica', 'trim|required|max_length[30]');
+        $this->form_validation->set_rules('fact_city', 'Fakturacne Mesto', 'trim|required|max_length[30]');
+        $this->form_validation->set_rules('fact_zip', 'Fakturacne PSC', 'trim|required|max_length[8]');
+        $this->form_validation->set_rules('fact_phone', 'Fakturacny Telefon', 'trim|required|max_length[20]');
+        $this->form_validation->set_rules('deliv_name', 'Dodacie Meno', 'max_length[30]');
+        $this->form_validation->set_rules('deliv_surname', 'Dodacie Meno', 'max_length[30]');
+        $this->form_validation->set_rules('deliv_company', 'Firma', 'max_length[30]');
+        $this->form_validation->set_rules('deliv_street', 'Dodacia Ulica', 'max_length[30]');
+        $this->form_validation->set_rules('deliv_city', 'Dodacie Mesto', 'max_length[30]');
+        $this->form_validation->set_rules('deliv_zip', 'Dodacie PSC', 'max_length[8]');
+        $this->form_validation->set_rules('deliv_info', 'Informacie', 'max_length[150]');
+        $this->form_validation->set_rules('deliv_phone', 'Dodaci Telefon', 'max_length[30]');
+        $this->form_validation->set_rules('comp_ico', 'ICO', 'max_length[30]');
+        $this->form_validation->set_rules('comp_dic', 'DIC', 'max_length[30]');
+        $this->form_validation->set_rules('comp_icdph', 'IC DPH', 'max_length[30]');
+        $this->form_validation->set_rules('comp_bic', 'BIC (SWIFT)', 'max_length[30]');
+        $this->form_validation->set_rules('comp_iban', 'IBAN', 'max_length[30]');
+        $this->form_validation->set_rules('comp_bank_owner', 'Majitel Uctu', 'max_length[40]');
+
+        $this->form_validation->set_message('valid_email', '%s musi obsahovat platny format.');
+        $this->form_validation->set_message('max_length', '%s nesmie mat viac ako 30 znakov.');
+        $this->form_validation->set_message('required', '%s je povinny udaj.');
+
+        if ($this->form_validation->run()) {
+            foreach ($this->input->post() as $key => $value) {
+                $this->session->set_userdata($key, $value);
+            }
+
+            redirect('ReviewAndPayment?id=3');
+        } else {
+            $this->session->set_flashdata('category_warning', validation_errors());
+            redirect('ShippingOptions?id=2');
+        }
+    }
 }
--- application/views/ShippingOptionsView.php
+++ application/views/ShippingOptionsView.php
@@ -1,9 +1,216 @@
 <div class="col-md-9 col-sm-offset-3">
     <?php $this->load->view('FlashMessagesView'); ?>
-    <a href="<?php echo base_url('ShippingAndBilling?id=1'); ?>" class="btn btn-default">
-        <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Spat
-    </a>
-    <a style="float: right" href="<?php echo base_url('ReviewAndPayment?id=3'); ?>" class="btn btn-success">Dalej
-        <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
-    </a>
+    <div class="panel-body">
+        <hr>
+        <p><b>Fakturačné údaje</b></p>
+        <?php echo form_open('ShippingOptions/checkShippingOptions', ['id' => 'form_shipping_options', 'class' => 'form-horizontal', 'role' => 'form']); ?>
+        <div class="required-fields">
+            <div class="form-group">
+                <label for="email" class="col-md-3 control-label">Email</label>
+                <div class="col-md-7">
+                    <input type="email" class="form-control" name="email"
+                           placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="fact_name" class="col-md-3 control-label">Meno</label>
+                <div class="col-md-7">
+                    <input type="text" class="form-control" name="fact_name"
+                           placeholder="Meno">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="fact_surname" class="col-md-3 control-label">Priezvisko</label>
+                <div class="col-md-7">
+                    <input type="text" class="form-control" name="fact_surname"
+                           placeholder="Priezvisko">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="fact_street" class="col-md-3 control-label">Ulica</label>
+                <div class="col-md-7">
+                    <input type="text" class="form-control street-search" name="fact_street"
+                           placeholder="Ulica">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="fact_city" class="col-md-3 control-label">Mesto</label>
+                <div class="col-md-7">
+                    <input type="text" onmouseover="zipForCity('city-search', 'zip-search_fact')"
+                           class="form-control city-search"
+                           name="fact_city" placeholder="Mesto">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="fact_zip" class="col-md-3 control-label">PSČ</label>
+                <div class="col-md-7">
+                    <input type="text" class="form-control zip-search_fact" name="fact_zip" placeholder="PSČ">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="fact_phone" class="col-md-3 control-label">Telefón</label>
+                <div class="col-md-7">
+                    <input type="text" class="form-control" name="fact_phone"
+                           placeholder="Telefón">
+                </div>
+            </div>
+        </div>
+        <div class="control-label">
+            <div class="checkbox">
+                <label><input type="checkbox" class="show-delivery-data" data-toggle="collapse"
+                              data-target="#delivery-data"> Zobrazit dodacie údaje</label>
+            </div>
+            <div id="delivery-data" class="collapse">
+                <hr>
+                <div class="form-group">
+                    <label class="col-md-6 control-label">Dodacie udaje. Vyplňte v prípade, že sa líšia od
+                        fakturačných.</label>
+                </div>
+                <div class="form-group">
+                    <label for="deliv_name" class="col-md-3 control-label">Meno</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="deliv_name" placeholder="Meno">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="deliv_surname" class="col-md-3 control-label">Priezvisko</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="deliv_surname" placeholder="Priezvisko">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="deliv_company" class="col-md-3 control-label">Firma</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="deliv_company" placeholder="Firma">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="deliv_street" class="col-md-3 control-label">Ulica</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control street-search" name="deliv_street" placeholder="Ulica">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="deliv_city" class="col-md-3 control-label">Mesto</label>
+                    <div class="col-md-7">
+                        <input type="text" onmouseover="zipForCity('deliv-city-search', 'zip-search_deliv')"
+                               class="form-control deliv-city-search" name="deliv_city" placeholder="Mesto">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="deliv_zip" class="col-md-3 control-label">PSČ</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control zip-search_deliv" name="deliv_zip" placeholder="PSČ">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="deliv_info" class="col-md-3 control-label">Informacie</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="deliv_info"
+                               placeholder="Informacie napr.: poschodie, cas...">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="deliv_phone" class="col-md-3 control-label">Telefón</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="deliv_phone"
+                               placeholder="Kontaktny telefon pre dopravcu">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="control-label">
+            <div class="checkbox" style="padding-bottom: 7px">
+                <label><input type="checkbox" class="show-company-data" data-toggle="collapse"
+                              data-target="#company-data"> Zobrazit firemné údaje</label>
+            </div>
+            <div id="company-data" class="collapse">
+                <hr>
+                <div class="form-group">
+                    <label class="col-md-6 control-label">Firemne udaje. Nezabudnite vyplniť IČO a IČ DPH, ak ste
+                        firma.</label>
+                </div>
+                <div class="form-group">
+                    <label for="comp_ico" class="col-md-3 control-label">IČO</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="comp_ico"
+                               placeholder="IČO">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="comp_dic" class="col-md-3 control-label">DIČ</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="comp_dic"
+                               placeholder="DIČ">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="comp_icdph" class="col-md-3 control-label">IČ DPH</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="comp_icdph" placeholder="IČ DPH">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="comp_bic" class="col-md-3 control-label">BIC (SWIFT)</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="comp_bic" placeholder="BIC (SWIFT)">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="comp_iban" class="col-md-3 control-label">IBAN</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="comp_iban" placeholder="IBAN">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="comp_bank_owner" class="col-md-3 control-label">Meno majiteľa bankového
+                        účtu</label>
+                    <div class="col-md-7">
+                        <input type="text" class="form-control" name="comp_bank_owner"
+                               placeholder="Meno majiteľa bankového účtu">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="<?php echo base_url('ShippingAndBilling?id=1'); ?>" class="btn btn-default">
+            <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Spat
+        </a>
+        <button style="float: right" type="submit" class="btn btn-success">Dalej
+            <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+        </button>
+        <?php echo form_close(); ?>
+    </div>
 </div>
+<script>
+    $(function () {
+        $(".city-search").autocomplete({
+            source: 'Registration/searchCity'
+        });
+        $(".deliv-city-search").autocomplete({
+            source: 'Registration/searchCity'
+        });
+        $(".zip-search").autocomplete({
+            source: 'Registration/searchZip'
+        });
+        $(".street-search").autocomplete({
+            source: 'Registration/searchStreet'
+        });
+    })
+</script>
+<script>
+    function zipForCity(city, factOrDelivZip) {
+        $('.' + city).on("autocompletechange", function () {
+            var searchTerm = {
+                city: $('.' + city).val()
+            };
+            $.ajax({
+                url: 'Registration/searchZipForCity',
+                type: 'GET',
+                data: searchTerm,
+                success: function (data) {
+                    $('.' + factOrDelivZip).val(data);
+                }
+            })
+        });
+    }
+</script>
