c0f26ccdac05f103e808fa77c9c655185c52b67d
matandre123 <matus.andrejcak@t-systems.sk>
2017-01-20 13:50:08 +0100

    [*] fix created separated js file for scripts

--- application/views/CheckoutView.php
+++ application/views/CheckoutView.php
@@ -9,44 +9,6 @@
         </ul>
     </div>
 </div>
-
-<script>
-    $(document).ready(function () {
-        switch (<?php echo $orderStep ?>) {
-            case 0:
-                $('#cart_order').removeClass().addClass('active');
-                $('#shipping_billing').removeClass().addClass('next');
-                $('#shipping_options').removeClass().addClass('next');
-                $('#review_payment').removeClass().addClass('next');
-                $('#complete_order').removeClass().addClass('next');
-                break;
-            case 1:
-                $('#cart_order').removeClass().addClass('previous visited');
-                $('#shipping_billing').removeClass().addClass('active');
-                $('#shipping_options').removeClass().addClass('next');
-                $('#review_payment').removeClass().addClass('next');
-                $('#complete_order').removeClass().addClass('next');
-                break;
-            case 2:
-                $('#cart_order').removeClass().addClass('previous visited');
-                $('#shipping_billing').removeClass().addClass('previous visited');
-                $('#shipping_options').removeClass().addClass('active');
-                $('#review_payment').removeClass().addClass('next');
-                $('#complete_order').removeClass().addClass('next');
-                break;
-            case 3:
-                $('#cart_order').removeClass().addClass('previous visited');
-                $('#shipping_billing').removeClass().addClass('previous visited');
-                $('#shipping_options').removeClass().addClass('previous visited');
-                $('#review_payment').removeClass().addClass('active');
-                $('#complete_order').removeClass().addClass('next');
-                break;
-            case 4:
-                $('#cart_order').removeClass().addClass('previous visited');
-                $('#shipping_billing').removeClass().addClass('previous visited');
-                $('#shipping_options').removeClass().addClass('previous visited');
-                $('#review_payment').removeClass().addClass('previous visited');
-                $('#complete_order').removeClass().addClass('active');
-        }
-    });
+<script type="text/javascript">
+    checkoutStep(<?php echo $orderStep ?>);
 </script>
---
+++ assets/js/checkout.js
@@ -0,0 +1,40 @@
+function checkoutStep(step) {
+    $(document).ready(function () {
+        switch (step) {
+            case 0:
+                $('#cart_order').removeClass().addClass('active');
+                $('#shipping_billing').removeClass().addClass('next');
+                $('#shipping_options').removeClass().addClass('next');
+                $('#review_payment').removeClass().addClass('next');
+                $('#complete_order').removeClass().addClass('next');
+                break;
+            case 1:
+                $('#cart_order').removeClass().addClass('previous visited');
+                $('#shipping_billing').removeClass().addClass('active');
+                $('#shipping_options').removeClass().addClass('next');
+                $('#review_payment').removeClass().addClass('next');
+                $('#complete_order').removeClass().addClass('next');
+                break;
+            case 2:
+                $('#cart_order').removeClass().addClass('previous visited');
+                $('#shipping_billing').removeClass().addClass('previous visited');
+                $('#shipping_options').removeClass().addClass('active');
+                $('#review_payment').removeClass().addClass('next');
+                $('#complete_order').removeClass().addClass('next');
+                break;
+            case 3:
+                $('#cart_order').removeClass().addClass('previous visited');
+                $('#shipping_billing').removeClass().addClass('previous visited');
+                $('#shipping_options').removeClass().addClass('previous visited');
+                $('#review_payment').removeClass().addClass('active');
+                $('#complete_order').removeClass().addClass('next');
+                break;
+            case 4:
+                $('#cart_order').removeClass().addClass('previous visited');
+                $('#shipping_billing').removeClass().addClass('previous visited');
+                $('#shipping_options').removeClass().addClass('previous visited');
+                $('#review_payment').removeClass().addClass('previous visited');
+                $('#complete_order').removeClass().addClass('active');
+        }
+    });
+}
