2d9e43a2a10fa6728d8cb6a28ce8454e9c72dd00
Matus Andrejcak <matus.andrejcak@t-systems.sk>
2017-03-17 15:09:22 +0100

    [*] slider update

--- application/views/HomeBodyView.php
+++ application/views/HomeBodyView.php
@@ -19,22 +19,14 @@
         </ol>
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-                <img src="<?php echo base_url('assets/img/pc-optimazation.jpg'); ?>" alt="Chania" width="460"
-                     height="345">
-                <div class="carousel-caption">
-                    <h3>Nazov</h3>
-                    <p>popis.</p>
-                </div>
+                <img src="<?php echo base_url('assets/img/pc-optimazation.jpg'); ?>" alt="Chania" width="1000"
+                     height="500">
+            </div>
+            <div class="item">
+                <img src="<?php echo base_url('assets/img/slider.jpg'); ?>" alt="Chania" width="1000"
+                     height="500">
             </div>
         </div>
-        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
     </div>
     <div class="col-md-12" id="products"></div>
     <div class="modal fade" id="modal" data-backdrop="static" data-keyboard="false">
